a251747cfe85ae5b32d35203ef348a588bcc4b4f
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-03-06 11:49:03 -0600

    feat: nuevo icono para imprimir ezterno

--- extencion_print_orion/trabajos_activos/paginas/trabajoActivoPaginasBodega.js
+++ extencion_print_orion/trabajos_activos/paginas/trabajoActivoPaginasBodega.js
@@ -1,67 +1,68 @@
-console.log('[Trabajos Activos Print Paginas]');
+console.log("[Trabajos Activos Print Paginas]");
 
 async function main() {
-  try {
-    const linkSelected = document.querySelector(
-      '#RMReportes > ul > li:nth-child(2) > div > ul > li:nth-child(3) > a'
-    );
+	try {
+		const linkSelected = document.querySelector("#RMReportes > ul > li:nth-child(2) > div > ul > li:nth-child(3) > a");
 
-    if (linkSelected) {
-      // Verifica si el elemento tiene la clase 'rmSelected'
-      const selected = linkSelected.classList.contains('rmSelected');
+		if (linkSelected) {
+			// Verifica si el elemento tiene la clase 'rmSelected'
+			const selected = linkSelected.classList.contains("rmSelected");
 
-      // Lanza un error si no se encuentra la clase
-      if (!selected) {
-        throw new Error('No se encontró el reporte seleccionado');
-      }
-    } else {
-      // Lanza un error si el elemento no existe
-      throw new Error('No se encontró el elemento especificado');
-    }
+			// Lanza un error si no se encuentra la clase
+			if (!selected) {
+				throw new Error("No se encontró el reporte seleccionado");
+			}
+		} else {
+			// Lanza un error si el elemento no existe
+			throw new Error("No se encontró el elemento especificado");
+		}
 
-    addClassToElement('html', 'impresion-active');
-    await insertElementPrintPaginas();
+		addClassToElement("html", "impresion-active");
+		await insertElementPrintPaginas();
 
-    setEventForPintPaginas();
-  } catch (error) {
-    console.error('Error:', error);
-  } finally {
-    alertPrintTrabajosActivos();
-  }
+		setEventForPintPaginas();
+	} catch (error) {
+		console.error("Error:", error);
+	} finally {
+		alertPrintTrabajosActivos();
+	}
 }
 
 function setEventForPintPaginas() {
-  // Escucha el evento clic en el botón print
-  const printButton = document.getElementById('printButtonTrabajoActivo');
-  if (!printButton) return;
-  printButton.addEventListener('click', getDataForToPrint);
+	// Escucha el evento clic en el botón print
+	const printButton = document.getElementById("printButtonTrabajoActivo");
+	if (!printButton) return;
+	printButton.addEventListener("click", getDataForToPrint);
 }
 
 function insertElementPrintPaginas() {
-  return new Promise((resolve, reject) => {
-    const buttonPrint = `
+	return new Promise((resolve, reject) => {
+		const buttonPrint = /*html*/ `
       <div class="p-2 bd-highlight">
-        <button id="printButtonTrabajoActivo" type="button" class="btn btn-sm text-grey btn-purple mt-3"><i class="fas fa-print"></i>Imprimir</button>
+        <button id="printButtonTrabajoActivo" type="button" class="btn btn-sm text-grey btn-purple mt-3">
+        ${iconPrintSend}
+        Imprimir
+        </button>
       </div>
       `;
 
-    const elementoForToInsert = document.querySelector(
-      '#Panel15 > main > div.row > div > div.d-flex.bd-highlight.mb-3'
-    );
+		const elementoForToInsert = document.querySelector(
+			"#Panel15 > main > div.row > div > div.d-flex.bd-highlight.mb-3"
+		);
 
-    const elementDelete = document.querySelector(
-      '#Panel15 > main > div.row > div > div.d-flex.bd-highlight.mb-3 > div:nth-child(1)'
-    );
+		const elementDelete = document.querySelector(
+			"#Panel15 > main > div.row > div > div.d-flex.bd-highlight.mb-3 > div:nth-child(1)"
+		);
 
-    elementDelete && elementDelete.remove();
+		elementDelete && elementDelete.remove();
 
-    if (elementoForToInsert) {
-      elementoForToInsert.insertAdjacentHTML('beforeend', buttonPrint);
-      resolve();
-    } else {
-      reject('No se encontro el elemento a insertar');
-    }
-  });
+		if (elementoForToInsert) {
+			elementoForToInsert.insertAdjacentHTML("beforeend", buttonPrint);
+			resolve();
+		} else {
+			reject("No se encontro el elemento a insertar");
+		}
+	});
 }
 
-window.addEventListener('load', main, { once: true });
+window.addEventListener("load", main, { once: true });
--- extencion_print_orion/trabajos_activos/visualizador/trabajosActivoVisualizador.js
+++ extencion_print_orion/trabajos_activos/visualizador/trabajosActivoVisualizador.js
@@ -1,63 +1,62 @@
-console.log('[Trbajos Activos Print Visualizador]');
+console.log("[Trbajos Activos Print Visualizador]");
 
 async function main() {
-  try {
-    const linkSelected = document.querySelector(
-      '#RMReportes > ul > li:nth-child(2) > div > ul > li.rmItem.rmLast > a'
-    );
+	try {
+		const linkSelected = document.querySelector("#RMReportes > ul > li:nth-child(2) > div > ul > li.rmItem.rmLast > a");
 
-    if (linkSelected) {
-      // Verifica si el elemento tiene la clase 'rmSelected'
-      const selected = linkSelected.classList.contains('rmSelected');
+		if (linkSelected) {
+			// Verifica si el elemento tiene la clase 'rmSelected'
+			const selected = linkSelected.classList.contains("rmSelected");
 
-      // Lanza un error si no se encuentra la clase
-      if (!selected) {
-        throw new Error('No se encontró el reporte seleccionado');
-      }
-    } else {
-      // Lanza un error si el elemento no existe
-      throw new Error('No se encontró el elemento especificado');
-    }
+			// Lanza un error si no se encuentra la clase
+			if (!selected) {
+				throw new Error("No se encontró el reporte seleccionado");
+			}
+		} else {
+			// Lanza un error si el elemento no existe
+			throw new Error("No se encontró el elemento especificado");
+		}
 
-    addClassToElement('html', 'impresion-active');
-    await insertElementPrintVisualizador();
+		addClassToElement("html", "impresion-active");
+		await insertElementPrintVisualizador();
 
-    setEventForPintVisualizador();
-  } catch (error) {
-    console.error(error);
-  } finally {
-    alertPrintTrabajosActivos();
-  }
+		setEventForPintVisualizador();
+	} catch (error) {
+		console.error(error);
+	} finally {
+		alertPrintTrabajosActivos();
+	}
 }
 
 function setEventForPintVisualizador() {
-  // Escucha el evento clic en el botón print
-  const printButton = document.getElementById('printButtonTrabajoActivo');
-  if (!printButton) return;
-  printButton.addEventListener('click', getDataForToPrint);
+	// Escucha el evento clic en el botón print
+	const printButton = document.getElementById("printButtonTrabajoActivo");
+	if (!printButton) return;
+	printButton.addEventListener("click", getDataForToPrint);
 }
 
 function insertElementPrintVisualizador() {
-  return new Promise((resolve, reject) => {
-    const buttonPrint = `
+	return new Promise((resolve, reject) => {
+		const buttonPrint = /*html*/ `
       <div class="d-flex bd-highlight">
         <div class="p-2 bd-highlight">
-          <button id="printButtonTrabajoActivo" type="button" class="btn btn-sm text-grey btn-purple mt-3"><i class="fas fa-print" aria-hidden="true"></i>Imprimir</button>
+          <button id="printButtonTrabajoActivo" type="button" class="btn btn-sm text-grey btn-purple mt-3">
+          ${iconPrintSend}
+          Imprimir
+          </button>
         </div>
       </div>
       `;
 
-    const elementoForToInsert = document.querySelector(
-      '#Panel15 > main > div.d-flex.justify-content-center'
-    );
+		const elementoForToInsert = document.querySelector("#Panel15 > main > div.d-flex.justify-content-center");
 
-    if (elementoForToInsert) {
-      elementoForToInsert.insertAdjacentHTML('afterend', buttonPrint);
-      resolve();
-    } else {
-      reject('No se encontro el elemento a insertar');
-    }
-  });
+		if (elementoForToInsert) {
+			elementoForToInsert.insertAdjacentHTML("afterend", buttonPrint);
+			resolve();
+		} else {
+			reject("No se encontro el elemento a insertar");
+		}
+	});
 }
 
-window.addEventListener('load', main, { once: true });
+window.addEventListener("load", main, { once: true });
